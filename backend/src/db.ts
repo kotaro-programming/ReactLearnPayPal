f12377e6b4a8c6e59c500c1bd5e86fefb58ab2a3
Kotaro Nakamura <>
2026-02-25 22:52:02 +0900

    Prisma adapter導入とdb初期化の移行

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,11 +1,13 @@
-import { Pool } from "pg";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-//本来は環境変数で管理する
-export const pool = new Pool({
-    user: "",
-    host: "",
-    database: "",
-    password: "",
-    port: 5432,
-});
+const globalForPrisma = globalThis as unknown as {
+    prisma?: PrismaClient;
+};
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+export const prisma =  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production"){
+    globalForPrisma.prisma = prisma;
+}
